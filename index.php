b3035073059800b621df8b81ff46283f9985f48f
termik88 <dirkser@mail.ru>
2018-02-01 00:24:40 +0500

    Знакомство с PHP

--- index.php
+++ index.php
@@ -27,7 +27,23 @@ $user_avatar = 'img/user.jpg';
         <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
+            <?php if ($is_auth): ?>
+            <div class="user-menu__image">
+                <img src="<?php echo $user_avatar ?>" width="40" height="40" alt="Пользователь">
+            </div>
+            <div class="user-menu__logged">
+                <p><?php echo $user_name ?></p>
+            </div>
+            <?php else: ?>
+                <ul class="user-menu__list">
+                    <li class="user-menu__item">
+                        <a href="#">Регистрация</a>
+                    </li>
+                    <li class="user-menu__item">
+                        <a href="#">Вход</a>
+                    </li>
+                </ul>
+            <?php endif; ?>
         <!-- здесь должен быть PHP код для показа аватара пользователя -->
 
         </nav>
