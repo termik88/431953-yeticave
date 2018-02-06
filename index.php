aad5a7e58ce5f7486efb65c4ae1062df14dff731
termik88 <dirkser@mail.ru>
2018-02-06 12:23:16 +0500

    Правки

--- index.php
+++ index.php
@@ -4,44 +4,44 @@ $is_auth = (bool) rand(0, 1);
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
 
-$menu_array_categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 
 $ad_array_items = [
     [
-        'Название' => '2014 Rossignol District Snowboard',
-        'Категория' => 'Доски и лыжи',
-        'Цена' => '10999',
-        'URL картинки' => 'img/lot-1.jpg'
+        'title' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => 10999,
+        'image_url' => 'img/lot-1.jpg'
     ],
     [
-        'Название' => 'DC Ply Mens 2016/2017 Snowboard',
-        'Категория' => 'Доски и лыжи',
-        'Цена' => '159999',
-        'URL картинки' => 'img/lot-2.jpg'
+        'title' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => 159999,
+        'image_url' => 'img/lot-2.jpg'
     ],
     [
-        'Название' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'Категория' => 'Крепления',
-        'Цена' => '8000',
-        'URL картинки' => 'img/lot-3.jpg'
+        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => 8000,
+        'image_url' => 'img/lot-3.jpg'
     ],
     [
-        'Название' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'Категория' => 'Ботинки',
-        'Цена' => '10999',
-        'URL картинки' => 'img/lot-4.jpg'
+        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => 10999,
+        'image_url' => 'img/lot-4.jpg'
     ],
     [
-        'Название' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'Категория' => 'Одежда',
-        'Цена' => '7500',
-        'URL картинки' => 'img/lot-5.jpg'
+        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => 7500,
+        'image_url' => 'img/lot-5.jpg'
     ],
     [
-        'Название' => 'Маска Oakley Canopy',
-        'Категория' => 'Разное',
-        'Цена' => '5400',
-        'URL картинки' => 'img/lot-6.jpg'
+        'title' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => 5400,
+        'image_url' => 'img/lot-6.jpg'
     ]
 ];
 ?>
@@ -126,12 +126,12 @@ $ad_array_items = [
                         <img src="<?= $value['URL картинки'] ?>" width="350" height="260" alt="Сноуборд">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?= $value['Категория'] ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $value['Название'] ?></a></h3>
+                        <span class="lot__category"><?= $value['category'] ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $value['title'] ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?= $value['Цена'] ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?= $value['price'] ?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
 
@@ -147,7 +147,7 @@ $ad_array_items = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <?php foreach ($menu_array_categories as $key => $value): ?>
+            <?php foreach ($categories as $key => $value): ?>
                 <li class="nav__item">
                     <a href="all-lots.html"><?= $value ?></a>
                 </li>
